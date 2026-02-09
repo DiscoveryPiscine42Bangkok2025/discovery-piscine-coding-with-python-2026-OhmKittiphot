cea5a9a24bbacfa76ae4a673d3772b021d87a006
RomieOhm <66070239@kmitl.ac.th>
2026-02-09 21:02:23 +0700

    finished 1.03

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,6 @@
+def urname(name, lastname):
+    first = name
+    last = lastname
+    print("Well, pleased to meet you," , first, last)
+
+urname(input("Hey, what's your first name? :"), input("And your lastname? :"))
