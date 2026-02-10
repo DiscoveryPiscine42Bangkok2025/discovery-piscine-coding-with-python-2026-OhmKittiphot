981ac6053ea886e9fc5efda050eb70641e0d7603
Kittiphot Mongkolrat <kittiphotmongkolrat@RomieOhms-MacBook-Air.local>
2026-02-10 11:25:16 +0700

    finished 4.02

---
+++ cell04/ex02/caluculator.py
@@ -0,0 +1,7 @@
+def cal(num1, num2):
+    print("Thank you!")
+    print(f"{num1} + {num2} = {num1 + num2}")
+    print(f"{num1} - {num2} = {num1 - num2}")
+    print(f"{num1} / {num2} = {num1 // num2}")
+    print(f"{num1} * {num2} = {num1 * num2}")
+cal(int(input("Give me the first number: ")), int(input("Give me the secound number: ")))
