c271608f90cb6d0b60b9d912f25fc77713b70df9
RomieOhm <66070239@kmitl.ac.th>
2026-02-11 20:21:36 +0700

    add 5.1-5.4

---
+++ cell05/ex03/play_with_arrys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+    
+    OriginalArray = [2, 8, 9, 48, 8, 22, -12, 2]
+    NewArry = {x + 2 for x in OriginalArray if x > 5}
+    
+    print(f"Original array: {OriginalArray}")
+    print(f"New array: {NewArry}")
+
+main()
