b6960e399cc93cdf9b041b76f9fa66f6e7ccc346
RomieOhm <66070239@kmitl.ac.th>
2026-02-11 23:55:13 +0700

    5.7-5.11

---
+++ cell05/ex08/aff_rev_params.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+
+def main():
+    params = sys.argv[1:]
+    if len(params) < 2:
+        print("none")
+    else:
+
+        for p in reversed(params):
+            print(p)
+main()
---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import sys
+import re
+
+def main():
+
+    if len(sys.argv) == 3:
+        keyword = sys.argv[1]
+        text = sys.argv[2]
+        matches = re.findall(keyword, text)
+        
+        if matches:
+            print(len(matches))
+        else:
+            print("none")
+    else:
+        print("none")
+
+main()
